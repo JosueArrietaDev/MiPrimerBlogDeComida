9e1cd13d835801dfd17a6ae064398800738416da
Josué Arrieta <166760183+josArr2024@users.noreply.github.com>
2024-07-05 15:07:08 -0300

    VOY A EMPEZAR A MOVER COSAS DE LUGAR PARTA DESPUES ORDENAR TODO Y METER EXPRESS

---
+++ BarraDeSeleccion.js
@@ -0,0 +1,36 @@
+    // ESTO ES INICIO Y LINCKS DE LA BARRA DE SELECCION
+// Obtener los enlaces de la barra de navegación AGREGAR LOS LINMCK
+
+var inicioLink = document.getElementById("inicioLink");
+var menuLink = document.getElementById("menuLink");
+var contactoLink = document.getElementById("contactoLink");
+var quienessomosLink = document.getElementById("quienessomosLink");
+
+// Asignar eventos de clic a los enlaces de navegación
+inicioLink.addEventListener("click", function (event) {
+  // Previene la acción predeterminada del enlace
+  event.preventDefault();
+  // Muestra la sección "Inicio"
+  mostrarSeccion("inicio");
+});
+
+munuLink.addEventListener("click", function (event) {
+  // Previene la acción predeterminada del enlace
+  event.preventDefault();
+  // Muestra la sección "menu"
+  mostrarSeccion("Menu");
+});
+
+quienessomosLink.addEventListener("click", function (event) {
+  // Previene la acción predeterminada del enlace
+  event.preventDefault();
+  // Muestra la sección "quienes somos"
+  mostrarSeccion("quienessomosLink");
+});
+
+contactoLink.addEventListener("click", function (event) {
+  // Previene la acción predeterminada del enlace
+  event.preventDefault();
+  // Muestra la sección "Contacto"
+  mostrarSeccion("contacto");
+});
